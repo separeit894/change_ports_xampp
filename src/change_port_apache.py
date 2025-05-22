baebb0179337db0705420371f81d06b6dff7ce86
separeit894 <rinatramazanov894@gmail.com>
2025-05-22 16:50:55 +0300

    Update-V_1.1

--- src/change_port_apache.py
+++ src/change_port_apache.py
@@ -1,34 +1,57 @@
 import os
 import sys
-# new_port = sys.argv[1]
+
 
 def change_port_apache(new_port):
+    # Cначала считываю файл, чтобы сделать backup
     with open("apache/conf/httpd.conf", 'r', encoding="utf-8") as file:
         src = file.readlines()
 
+    # Если нету папки backup, то он её создает
     if not os.path.exists("backup"):
         os.makedirs("backup")
-
+    
+    """ 
+    Если до этого порты менялись, то уже есть первоначальный бэкап,
+    и заменятся он не будет. Это делается для того чтобы сохранить рабочий вариант файла,
+    перезаписывание возможно приведет к неисправной обработке файла xampp
+    """
     if not os.path.exists("backup/httpd.conf"):
         with open("backup/httpd.conf", "w", encoding="utf-8") as file:
             file.writelines(src)
 
+    # Создаем переменную в которую будем вводить порт
     index_port_listen = None
 
+    """
+    Проходясь по циклу, скрипт ищет строку #Listen
+    Чтобы затем заменить порт
+    """
     for i, line in enumerate(src):
+        # Проверям не начинается строка с #, в ином случае просто игнорируем строку
         if not line.startswith("#"):
+            # Если # нету, то он ищет слово Listen
             if "Listen" in src[i]:
                 print(i, line)
+                # Присваеваем переменную номер строки файла
                 index_port_listen = i
 
+    # Меняем порт
     src[index_port_listen] = f"Listen {new_port}\n"
 
+    # Создаем вторую переменную, в которую позже будем хранить новое значения порта
     index_port_servername = None
-
+    """
+    Цикл считывает строки файла,
+    в if проверяется не начинается файл с #, 
+    если да, то он пропускает строку
+    если нет, то он продолжает считывать строку, иская слово ServerName
+    """
     for i, line in enumerate(src):
         if not line.startswith("#"):
             if "ServerName" in src[i]:
                 print(i, line)
+                # Присваевам номер строки файла в переменную
                 index_port_servername = i
 
     src[index_port_servername] = src[index_port_servername].split(":")
@@ -37,6 +60,7 @@ def change_port_apache(new_port):
 
     src[index_port_servername] = result
 
+    # Сохраняем уже измененный файл
     with open("apache/conf/httpd.conf", "w", encoding="utf-8") as file:
         file.writelines(src)
 
--- src/change_ssl.py
+++ src/change_ssl.py
@@ -1,46 +1,74 @@
 import os
 import sys
-# new_port = sys.argv[1]
+
 
 def change_port_ssl(new_port):
+    
+    # Сначала открываю файл, чтобы сделать его backup
     with open("apache/conf/extra/httpd-ssl.conf", "r", encoding="utf-8") as file:
         src = file.readlines()
-
+    # Если нету папки backup, то он её создает
     if not os.path.exists("backup"):
         os.makedirs("backup")
-
+    # Если нету резервного файла, то он его создает
     if not os.path.exists("backup/httpd-ssl.conf"):
         with open("backup/httpd-ssl.conf", "w", encoding="utf-8") as file:
             file.writelines(src)
-
+    
+    # Создаем переменную, в которой позже будем хранить значения нового порта
     index_listen = None
+
+    """
+    Цикл считывает файл, проходит по каждой строке
+    Внешнее условие проверяет - не начинается строка #, 
+    если он начинается #, то он пропускает строку файла
+    если нет, он начинает искать слово Listen,
+    и если он его находит, присваивает значения номера строки в переменную
+    """
     for i, line in enumerate(src):
         if not line.startswith("#"):
             if "Listen" in src[i]:
                 print(i, line)
                 index_listen = i
 
+    # В этой строке уже вводится новый порт, который ввел пользователь
     src[index_listen] = f"Listen {new_port}\n"
 
-
+    # Новая переменная, которая будет хранить порт
     index_virtualhost = None
+
+    """
+    Тот же цикл, что и с 28 - 32 строку кода,
+    Так же проверяет не начинается строка #
+    если да, пропускает строку
+    если нет, переходит к внутреннему условие 
+    Но он уже ищет <VirtualHost _default_:
+    После : мы уже ставим другой порт
+    """
     for i, line in enumerate(src):
         if not line.startswith("#"):
             if "<VirtualHost _default_:" in src[i]:
                 print(i, line)
                 index_virtualhost = i
 
+    # Присваеваем переменной новый порт
     src[index_virtualhost] = f"<VirtualHost _default_:{new_port}>\n"
 
+    # Переменная, которая будет хранить значение порта
     index_servername = None
+    """
+    Тот же самый цикл что и с 48 - 52 строку кода
+    """
     for i, line in enumerate(src):
         if not line.startswith("#"):
             if "ServerName" in src[i]:
                 print(i, line)
                 index_servername = i
 
+    # Присваеваем строке, новый порт
     src[index_servername] = f"ServerName www.example.com:{new_port}\n"
 
+    # Сохраняем изменный файл
     with open("apache/conf/extra/httpd-ssl.conf", "w", encoding="utf-8") as file:
         file.writelines(src)
 
