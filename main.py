c22b389f4a7d4b9200f8ac325a19627e81c43d5f
separeit894 <rinatramazanov894@gmail.com>
2025-05-27 15:30:00 +0300

    V-1.3

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ import tkinter as tk
 from src.change_port_apache import change_port_apache
 from src.change_ssl import change_port_ssl
 from src.mysql_and_phpmyadmin import change_port_mysql
+from src.edit_xampp_control import *
+
 
 root = Tk()
 root.title("Change Ports Xampp")
 # Создаю окно на 600x400
 root.geometry("600x400")
+
+
+result_port_mysql = None
+result_port_apache = None
+result_port_apachessl = None
 
 
 style = ttk.Style()
@@ -17,7 +24,7 @@ style.configure("Big.TButton", font=("Arial", 12), padding=10, width=20, height=
 style.configure("Small.TButton", font=("Arial", 10), padding=10, width=20, height=5)
 
 # Обозначение версии приложения
-text_version = ttk.Label(root, text="Версия: 1.2", font=("Arial", 8))
+text_version = ttk.Label(root, text="Версия: 1.3", font=("Arial", 8))
 text_version.pack(anchor=W)
 
 # Текст 'Главное меню', который будет использовать шрифт Arial 12 пунктов
@@ -28,6 +35,7 @@ main_label.pack(anchor=CENTER, padx=10, pady=30)
 # Apache
 
 def apache_button():
+    # global result_port_apache
     window = Toplevel(root)  # Используем Toplevel вместо Tk() для дочерних окон
     window.title("Меню изменения порта Apache")
     window.geometry("500x250")
@@ -42,6 +50,8 @@ def apache_button():
 
     # Функция, которая будеть работать если пользователь нажмет на кнопку 'Применить'
     def on_submit():
+        global result_port_apache
+        result_port_apache = str(enter_pole.get())
         # Выводит значение, которое пользователь ввел, после того как кликнул по кнопке "Применить"
         print("Вы ввели:", enter_pole.get())
         # Выводит строку с успешным изменением порта ( см. 41 строку кода )
@@ -49,7 +59,7 @@ def apache_button():
         # Через 3000 мс (3 секунды) удаляем надпись
         window.after(3000, result_label.pack_forget)
         # Передаем значение нового порта в функцию
-        change_port_apache(enter_pole.get())
+        change_port_apache(result_port_apache)
 
     """
     Кнопка submit_button, прикрепляется к окну window
@@ -71,6 +81,7 @@ button_apache.pack(anchor=CENTER, fill=tk.X, padx=100)
 # ApacheSSL
 
 def apachessl_button():
+    
     window = Toplevel(root)  # Используем Toplevel вместо Tk() для дочерних окон
     window.title("Меню изменения порта ApacheSSL")
     window.geometry("500x250")
@@ -85,6 +96,8 @@ def apachessl_button():
 
     # Функция, которая будеть работать если пользователь нажмет на кнопку 'Применить'
     def on_submit():
+        global result_port_apachessl
+        result_port_apachessl = str(enter_pole.get())
         # Выводит значение, которое пользователь ввел, после того как кликнул по кнопке "Применить"
         print("Вы ввели:", enter_pole.get())
         # Выводит строку с успешным изменением порта ( см. 84 строку кода )
@@ -92,7 +105,7 @@ def apachessl_button():
         # Через 3000 мс (3 секунды) удаляем надпись
         window.after(3000, result_label.pack_forget)
         # Передаем значение нового порта в функцию
-        change_port_ssl(enter_pole.get())
+        change_port_ssl(result_port_apachessl)
 
     """
     Кнопка 'Применить', прикрепляется к окну window.
@@ -114,6 +127,7 @@ button_apachessl.pack(anchor=CENTER, fill=tk.X, padx=100)
 # MySQL
 
 def mysql_button():
+    
     window = Toplevel(root)  # Используем Toplevel вместо Tk() для дочерних окон
     window.title("Меню изменения порта MySQL")
     window.geometry("500x250")
@@ -129,13 +143,16 @@ def mysql_button():
     # Функция, которая будеть работать если пользователь нажмет на кнопку 'Применить'
     def on_submit():
         # Выводит значение, которое пользователь ввел, после того как кликнул по кнопке "Применить"
+        global result_port_mysql
+
+        result_port_mysql = str(enter_pole.get())
         print("Вы ввели:", enter_pole.get())
         # Выводит строку с успешным изменением порта ( см. 124 строку кода )
         result_label.pack(anchor=CENTER, pady=20, padx=20)
         # Через 3000 мс (3 секунды) удаляем надпись
         window.after(3000, result_label.pack_forget)
         # Передаем значение нового порта в функцию
-        change_port_mysql(enter_pole.get())
+        change_port_mysql(result_port_mysql)
 
     """
     Кнопка 'Применить', прикрепляется к окну window.
@@ -153,6 +170,105 @@ def mysql_button():
 button_mysql = ttk.Button(root, text="MySQL", command=mysql_button, style="Big.TButton")
 # Кнопка будет размещена по центру, отступ по x 100, будет растягиваться по ширине
 button_mysql.pack(anchor=CENTER, fill=tk.X, padx=100)
+
+# Xampp-control.ini
+
+def edit_xampp_control_button():
+    window = Toplevel(root)  # Используем Toplevel вместо Tk() для дочерних окон
+    window.title("Меню изменения портов в xampp_control.ini")
+    window.geometry("500x300")
+
+    # Apache
+    Frame_Apache = ttk.Frame(window)
+    Frame_Apache.pack(pady=10)
+
+    Label_Apache = ttk.Label(Frame_Apache, text="Apache", width=10, font=("Arial", 12))
+    Label_Apache.pack(side=tk.LEFT)
+
+    # Поле ввода, прикрепляется к окну window, будет иметь шрифт Arial 16 пунктов
+    enter_pole_apache = ttk.Entry(Frame_Apache, width=20, font=("Arial", 16))
+    enter_pole_apache.pack(side=tk.LEFT)
+    # Значение, которое пользователь ввел ранее
+    enter_pole_apache.insert(0, str(result_port_apache))
+
+    # ApacheSSL
+    Frame_ApacheSSL = ttk.Frame(window)
+    Frame_ApacheSSL.pack(pady=10)
+
+    Label_ApacheSSL = ttk.Label(Frame_ApacheSSL, text="ApacheSSL", width=10, font=("Arial", 12))
+    Label_ApacheSSL.pack(side=tk.LEFT)
+
+    # Поле ввода, прикрепляется к окну window, будет иметь шрифт Arial 16 пунктов
+    enter_pole_apachessl = ttk.Entry(Frame_ApacheSSL, width=20, font=("Arial", 16))
+    # Размещается по центру
+    enter_pole_apachessl.pack(side=tk.LEFT)
+    enter_pole_apachessl.insert(0, str(result_port_apachessl))
+
+    # MySQL
+    Frame_MySQL = ttk.Frame(window)
+    Frame_MySQL.pack(pady=10)
+
+    Label_MySQL = ttk.Label(Frame_MySQL, text="MySQL", width=10, font=("Arial", 12))
+    Label_MySQL.pack(side=tk.LEFT)
+
+    # Поле ввода, прикрепляется к Frame_MySQL, будет иметь шрифт Arial 16 пунктов
+    enter_pole_mysql = ttk.Entry(Frame_MySQL, width=20, font=("Arial", 16))
+    enter_pole_mysql.pack(side=tk.LEFT)
+    enter_pole_mysql.insert(0, str(result_port_mysql))
+
+    
+
+    # Будет работать после того как пользователь кликнул по кнопке 'Применить'
+    result_label = ttk.Label(window, text="Порты изменены", font=("Arial", 12))
+
+
+    # Функция, которая будеть работать если пользователь нажмет на кнопку 'Применить'
+    def on_submit():
+        global result_port_apache, result_port_mysql, result_port_apachessl
+        # Выводит строку с успешным изменением порта ( см. 124 строку кода )
+        result_label.pack(anchor=CENTER, pady=20, padx=20)
+        # Через 3000 мс (3 секунды) удаляем надпись
+        window.after(3000, result_label.pack_forget)
+
+        # Если переменная пуста, то значение берется из того что пользователь ввел
+        if result_port_mysql is None:
+            result_port_mysql = str(enter_pole_mysql.get())
+
+        print(result_port_mysql)
+        
+        if result_port_apache is None:
+            result_port_apache = str(enter_pole_apache.get())
+
+        print(result_port_apache)
+
+        if result_port_apachessl is None:
+            result_port_apachessl = str(enter_pole_apachessl.get())
+
+        print(result_port_apachessl)
+
+        # Перезапуск программы в случае если нету прав администратора
+        if ctypes.windll.shell32.IsUserAnAdmin():
+            edit_file_xampp_control(str(result_port_mysql), str(result_port_apachessl), str(result_port_apache))
+        else:
+            print("Ошибка: Требуются права администратора.")
+            run_as_admin()
+
+    """
+    Кнопка 'Применить', прикрепляется к окну window.
+    Ссылается на функцию on_submit ( см. 228 строку кода )
+    Имеет стиль 'Small.TButton ( см. 17 строку кода )'
+    """
+    submit_button = ttk.Button(window, text="Применить", command=on_submit, style="Small.TButton")
+    submit_button.pack()
+
+"""
+Кнопка 'edit xampp control.ini', прикреплена к главному окну ( root )
+Ссылается на функцию 'edit_xampp_control_button' (см. код с 176 - 261 стр.)
+Используется стиль Big.TButton ( см. 16 стр )
+"""
+button_xampp_control = ttk.Button(root, text="Edit xampp control.ini\nТребуются права администратора!", command=edit_xampp_control_button, style="Big.TButton")
+# Кнопка будет размещена по центру, отступ по x 100, будет растягиваться по ширине
+button_xampp_control.pack(anchor=CENTER, fill=tk.X, padx=100)
 
 # Запускается цикл
 root.mainloop()
---
+++ src/edit_xampp_control.py
@@ -0,0 +1,80 @@
+import ctypes
+import sys
+import os
+import re
+
+
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+def run_as_admin():
+    ctypes.windll.shell32.ShellExecuteW(
+        None, "runas", sys.executable, os.path.abspath(sys.argv[0]), None, 1)
+    sys.exit(0)
+
+def edit_file_xampp_control(mysql_port, apachessl_port, apache_port):
+    print("Функция edit_file_xampp_control запущена!")
+
+    file_path = "xampp-control.ini"  # можно заменить на полный путь, если нужно
+
+    if not os.path.exists(file_path):
+        print(f"[Ошибка] Файл {file_path} не найден!")
+        return
+
+    with open(file_path, 'r', encoding="utf-8") as file:
+        lines = file.readlines()
+
+    in_section = False
+    apache_line_index = apachessl_line_index = mysql_line_index = None
+
+    for i, line in enumerate(lines):
+        line = line.strip()
+
+        if line == "[ServicePorts]":
+            in_section = True
+            continue
+
+        if in_section and line.startswith("["):
+            break  # выходим из раздела
+
+        if not in_section:
+            continue
+        # Находим строго слово "Apache" и так далее
+        if re.search(r'\bApache\b', line):
+            apache_line_index = i
+            result = line.split("=")
+            result[1] = f"={apache_port}"
+            line = "".join(result)
+            lines[i] = f"{line}\n"
+            print(f"Нашёл Apache на строке {i}: {lines[i]}")
+        elif re.search(r'\bApacheSSL\b', line):
+            apachessl_line_index = i
+            result = line.split("=")
+            result[1] = f"={apachessl_port}"
+            line = "".join(result)
+            lines[i] = f"{line}\n"
+            print(f"Нашёл ApacheSSL на строке {i}: {lines[i]}")
+        elif re.search(r'\bMySQL\b', line):
+            mysql_line_index = i
+            result = line.split("=")
+            result[1] = f"={mysql_port}"
+            line = "".join(result)
+            lines[i] = f"{line}\n"
+            print(f"Нашёл MySQL на строке {i}: {lines[i]}")
+
+    # Записываем изменения в файл
+    with open("xampp-control.ini", "w", encoding="utf-8") as file:
+        file.writelines(lines)
+    print("Запись файла")
+
+
+if __name__ == "__main__":
+    if ctypes.windll.shell32.IsUserAnAdmin():
+        edit_file_xampp_control()
+    else:
+        print("Ошибка: Требуются права администратора.")
+        run_as_admin()
